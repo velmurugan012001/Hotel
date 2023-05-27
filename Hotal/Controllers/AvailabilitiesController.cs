695f8d46fa68796b2245ff9113f45a7da56481be
velmurugan012001 <bvelmurugan01122001@gmai.com>
2023-05-27 21:55:10 +0530

    add controllers

---
+++ Hotal/Controllers/AvailabilitiesController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Hotal.Model;
+
+namespace Hotal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AvailabilitiesController : ControllerBase
+    {
+        private readonly HotalDbContext _context;
+
+        public AvailabilitiesController(HotalDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Availabilities
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Availability>>> GetAvailabilities()
+        {
+          if (_context.Availabilities == null)
+          {
+              return NotFound();
+          }
+            return await _context.Availabilities.ToListAsync();
+        }
+
+        // GET: api/Availabilities/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Availability>> GetAvailability(bool id)
+        {
+          if (_context.Availabilities == null)
+          {
+              return NotFound();
+          }
+            var availability = await _context.Availabilities.FindAsync(id);
+
+            if (availability == null)
+            {
+                return NotFound();
+            }
+
+            return availability;
+        }
+
+        // PUT: api/Availabilities/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAvailability(bool id, Availability availability)
+        {
+            if (id != availability.AvailabilityId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(availability).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AvailabilityExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Availabilities
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Availability>> PostAvailability(Availability availability)
+        {
+          if (_context.Availabilities == null)
+          {
+              return Problem("Entity set 'HotalDbContext.Availabilities'  is null.");
+          }
+            _context.Availabilities.Add(availability);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (AvailabilityExists(availability.AvailabilityId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetAvailability", new { id = availability.AvailabilityId }, availability);
+        }
+
+        // DELETE: api/Availabilities/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAvailability(bool id)
+        {
+            if (_context.Availabilities == null)
+            {
+                return NotFound();
+            }
+            var availability = await _context.Availabilities.FindAsync(id);
+            if (availability == null)
+            {
+                return NotFound();
+            }
+
+            _context.Availabilities.Remove(availability);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AvailabilityExists(bool id)
+        {
+            return (_context.Availabilities?.Any(e => e.AvailabilityId == id)).GetValueOrDefault();
+        }
+    }
+}
